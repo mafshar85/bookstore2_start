5ea9b2e6264c5cd305fbe83cdacc2d5f02a43417
ILIAa <mafshar85@hotmail.com>
2020-06-20 20:30:20 +0430

    create_model_init

---
+++ bookstore2/shop/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class BaseModel(models.Model):
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+    modify_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+       abstract  = True
+
+
+class Category(BaseModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(BaseModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+
+class BOOK(BaseModel):
+    COVER_HARD = 'hard'
+    COVER_SOFT = 'soft'
+    COVER_CHOICE = ((COVER_HARD, 'Hard Cover'),
+                    (COVER_SOFT, 'Soft Cover' ))
+    name = models.CharField(max_length=255)
+    publish_year = models.IntegerField(null=True, blank=True)
+    cover_type = models.CharField(null=True, blank=True,
+                                  choices=COVER_CHOICE,
+                                  max_length=5)
+    author = models.ForeignKey('Author', on_delete=models.PROTECT)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    tag = models.ManyToManyField(Tag)
+    price = models.IntegerField()
+    cover = models.ImageField(upload_to='File/book-covers/', null=True, blank=True)
+    pdf = models.FileField(upload_to='File/pdf/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}-({self.author})"
+
+
+class Author(BaseModel):
+    name = models.CharField(max_length=255)
+    birth = models.IntegerField(null=True)
+    death = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f"{self.name}-({self.birth})"
+
